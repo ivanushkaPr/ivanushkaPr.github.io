74d4a4f03e917800c56bf6301e0dec9b369fb0cc
Ivan Krasheninnikov <exitusletalis2008@yandex.ru>
2020-07-28 13:59:17 +0300

    Added todo app demonstration.

---
+++ todo/precache-manifest.01d84192ce1ba15036ba3c07b94ff718.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9faae6de7a4b848d91ce8b0e5298d316",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f001bccd797f9744959a",
+    "url": "./static/css/main.c345cb26.chunk.css"
+  },
+  {
+    "revision": "634c2a791e1d776a648a",
+    "url": "./static/js/2.08ee10f6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.08ee10f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f001bccd797f9744959a",
+    "url": "./static/js/main.5581da31.chunk.js"
+  },
+  {
+    "revision": "706c01f795c876eacc56",
+    "url": "./static/js/runtime-main.3cf45a73.js"
+  },
+  {
+    "revision": "6ef2b84790dbffc19d0e8bbfafbc861e",
+    "url": "./static/media/checked.6ef2b847.svg"
+  }
+]);
