1942fd7502cc2bdd338cc7709ce56b604a008c65
Ivan Krasheninnikov <exitusletalis2008@yandex.ru>
2020-08-10 15:03:50 +0300

    Added mvp version of gaysongs.

---
+++ gaysongs/precache-manifest.35a43c34c413f7ad701dcd749993ef98.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "574cf2749957d6017d84bbfab25ce69f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6fed154e186bac975c38",
+    "url": "./static/css/main.32720331.chunk.css"
+  },
+  {
+    "revision": "7e266164c6ce8e029574",
+    "url": "./static/js/2.2714be4b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.2714be4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fed154e186bac975c38",
+    "url": "./static/js/main.015496c5.chunk.js"
+  },
+  {
+    "revision": "58bac29d4ff3cfc69d22",
+    "url": "./static/js/runtime-main.a7835e1d.js"
+  },
+  {
+    "revision": "4f82cd73f3446ef1af7fa6d3a302ef3a",
+    "url": "./static/media/Logo.4f82cd73.svg"
+  },
+  {
+    "revision": "06a469380cb09a64d372f773665e7500",
+    "url": "./static/media/bird-mock.06a46938.jpg"
+  },
+  {
+    "revision": "9fe28c050c2b7fd62df914058e876278",
+    "url": "./static/media/pause.9fe28c05.svg"
+  },
+  {
+    "revision": "84f25a4dc1c7103777cd4689abc0d4ba",
+    "url": "./static/media/play.84f25a4d.svg"
+  },
+  {
+    "revision": "c59c1ab15f452eea5a38d496a08857e7",
+    "url": "./static/media/right.c59c1ab1.mp3"
+  },
+  {
+    "revision": "158e2549a6c73cda0a51cb585946d82d",
+    "url": "./static/media/wrong.158e2549.mp3"
+  }
+]);
