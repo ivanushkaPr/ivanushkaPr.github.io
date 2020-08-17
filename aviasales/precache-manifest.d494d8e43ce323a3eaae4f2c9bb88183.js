b1f7271ceb5c27bbec8e7d35b4db4f0acf8f6dfd
Ivan Krasheninnikov <exitusletalis2008@yandex.ru>
2020-08-17 18:12:22 +0300

    Added aviasales task.

---
+++ aviasales/precache-manifest.d494d8e43ce323a3eaae4f2c9bb88183.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "129e90bdf1c6ae76c839ca63166b4786",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e2e136fa857bb404a690",
+    "url": "./static/css/main.2b89e83f.chunk.css"
+  },
+  {
+    "revision": "bfb69e192036c05c77e8",
+    "url": "./static/js/2.1bbd7498.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.1bbd7498.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2e136fa857bb404a690",
+    "url": "./static/js/main.19322ecf.chunk.js"
+  },
+  {
+    "revision": "28f700559f7abdaab058",
+    "url": "./static/js/runtime-main.e3f54cdc.js"
+  },
+  {
+    "revision": "d2a4807b0aa4a7ca1e7a82ee730e6fb6",
+    "url": "./static/media/logo.d2a4807b.png"
+  }
+]);
